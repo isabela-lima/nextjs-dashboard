1b18a8f7e8483ae39c4bdd9199674d73dc480506
Isabela Lima <isabelamslimas@gmail.com>
2024-11-15 19:51:34 -0300

    feat: :sparkles: finished creating dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // * NOTE: This has a warning:
+  // ! CanaryOnlyError: The experimental feature "experimental.ppr" can only be enabled when using the latest canary version of Next.js.
+  // experimental: {
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
